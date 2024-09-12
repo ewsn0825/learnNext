de2049cd42b8fc25a0a0a7253b137af3ed99a744
ewsn0825 <ewsn0825@naver.com>
2024-09-12 20:24:50 +0900

    feat : layout 추가

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -3,9 +3,11 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 export default function Navigation() {
   const path = usePathname();
+  const [count, setCount] = useState(0);
   // console.log(path);
   return (
     <nav>
@@ -17,6 +19,10 @@ export default function Navigation() {
         <li>
           <Link href="/about-us">About Us</Link>{" "}
           {path === "/about-us" ? "시벌" : ""}
+        </li>
+        <li>
+          {/* 자바스크립트 비활성화 되어있으면 작동안됨 */}
+          <button onClick={() => setCount((c) => c + 1)}>{count}</button>
         </li>
       </ul>
     </nav>
