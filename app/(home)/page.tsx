79a9e832d8705ae0c82b887826aa6647e6bc8092
ewsn0825 <ewsn0825@naver.com>
2024-09-19 21:01:59 +0900

    feat : server component api 연동 구현

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,15 +1,35 @@
 // import Navigation from "../../components/navigation";
+// "use client";
+// import { useEffect, useState } from "react";
+
+import { link } from "fs";
+import Link from "next/link";
 
 export const metadata = {
   title: "Home",
   // description: "The best movies on the best framework",
 };
 
-export default function Page() {
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+async function getMovies() {
+  // await new Promise((resolve) => setTimeout(resolve, 10000));
+  //next.js는 패칭된 데이터를 캐싱해준다 (프레임워크이기 때문)
+  //server component에서 fetch 된걸 기억한다
+  const response = await fetch(API_URL);
+  const json = await response.json();
+  return json;
+}
+
+export default async function HomePage() {
+  const movies = await getMovies();
   return (
     <div>
-      {/* <Navigation /> */}
-      <h1>Hello!</h1>
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
     </div>
   );
 }
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,8 +1,18 @@
-export default function MovieDetail({
+import { API_URL } from "../../../(home)/page";
+
+async function getMovie(id: string) {
+  const response = await fetch(`${API_URL}/${id}`);
+  return response.json();
+}
+
+export default async function MovieDetail({
   params: { id },
 }: {
   params: { id: string };
 }) {
+  const movie = await getMovie(id);
   // console.log(props);
-  return <h1>Movie {id}</h1>;
+  return <h1>{movie.title}</h1>;
 }
+
+//리액트 라우터의 movies/:id와 같은 구조
