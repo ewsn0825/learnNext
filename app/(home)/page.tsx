43e975859f929200c6977b1f039f97d7b13a1529
ewsn0825 <ewsn0825@naver.com>
2024-09-27 18:20:39 +0900

    chore : 배포

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,20 +1,51 @@
-// import Navigation from "../../components/navigation";
-// "use client";
-// import { useEffect, useState } from "react";
+// // import Navigation from "../../components/navigation";
+// // "use client";
+// // import { useEffect, useState } from "react";
+
+// import Movie from "../../components/movie";
+// import { API_URL } from "../constants";
+// import styles from "../styles/home.module.css";
+
+// export const metadata = {
+//   title: "Home",
+//   // description: "The best movies on the best framework",
+// };
+
+// async function getMovies() {
+//   // await new Promise((resolve) => setTimeout(resolve, 10000));
+//   //next.js는 패칭된 데이터를 캐싱해준다 (프레임워크이기 때문)
+//   //server component에서 fetch 된걸 기억한다
+//   const response = await fetch(API_URL);
+//   const json = await response.json();
+//   return json;
+// }
+
+// export default async function HomePage() {
+//   const movies = await getMovies();
+//   return (
+//     <div className={styles.container}>
+//       {movies.map((movie) => (
+//         <Movie
+//           key={movie.id}
+//           id={movie.id}
+//           poster_path={movie.poster_path}
+//           title={movie.title}
+//         />
+//       ))}
+//     </div>
+//   );
+// }
 
 import Movie from "../../components/movie";
-import { API_URL } from "../constants";
 import styles from "../styles/home.module.css";
+import { API_URL } from "../constants";
 
 export const metadata = {
   title: "Home",
-  // description: "The best movies on the best framework",
 };
 
 async function getMovies() {
-  // await new Promise((resolve) => setTimeout(resolve, 10000));
-  //next.js는 패칭된 데이터를 캐싱해준다 (프레임워크이기 때문)
-  //server component에서 fetch 된걸 기억한다
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
   const response = await fetch(API_URL);
   const json = await response.json();
   return json;
@@ -35,3 +66,5 @@ export default async function HomePage() {
     </div>
   );
 }
+
+export const runtime = "edge";
